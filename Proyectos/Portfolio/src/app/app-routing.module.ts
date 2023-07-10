90339cd90a7273161d8f35f2de2928218e84625b
pabloariel413 <pabloariel413@gmail.com>
2023-07-09 21:26:47 -0300

    routing

--- Proyectos/Portfolio/src/app/app-routing.module.ts
+++ Proyectos/Portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WorksComponent } from './components/works/works.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'works',
+    component: WorksComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
